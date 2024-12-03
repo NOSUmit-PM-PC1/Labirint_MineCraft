03dad65bb944c62c0b5c78f6fd99acd693eb7fa2
Мария <mahadrum@yandex.ru>
2024-12-03 12:29:17 +0300

    Добавьте файлы проекта.

---
+++ Labirint/FormGame.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Labirint
+{
+    partial class FormGame
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewLabirint = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLabirint)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewLabirint
+            // 
+            this.dataGridViewLabirint.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewLabirint.ColumnHeadersVisible = false;
+            this.dataGridViewLabirint.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewLabirint.Name = "dataGridViewLabirint";
+            this.dataGridViewLabirint.RowHeadersVisible = false;
+            this.dataGridViewLabirint.RowTemplate.Height = 21;
+            this.dataGridViewLabirint.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewLabirint.Size = new System.Drawing.Size(240, 231);
+            this.dataGridViewLabirint.TabIndex = 0;
+            // 
+            // FormGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 231);
+            this.Controls.Add(this.dataGridViewLabirint);
+            this.Name = "FormGame";
+            this.Text = "Лабиринт";
+            this.Load += new System.EventHandler(this.FormGame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLabirint)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewLabirint;
+    }
+}
+
---
+++ Labirint/FormGame.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Labirint
+{
+    public partial class FormGame : Form
+    {
+        char[,] field;
+        Image fon, stone, steve;
+        int x = 1, y = 1;
+
+
+        char[,] LoadLevelFromFile(string filename)
+        {
+            
+            StreamReader reader = new StreamReader(filename);
+            string[] temp = reader.ReadLine().Split();
+            int rows = Convert.ToInt32(temp[0]);
+            int cols = Convert.ToInt32(temp[1]);
+            char[,] data = new char[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                string s = reader.ReadLine();
+                for (int j = 0; j < cols; j++)
+                    data[i, j] = s[j];
+            }
+            reader.Close();
+            return data;
+        }
+
+        void ShowField(char[,] data)
+        {
+            int widthColumn = 60;
+
+            dataGridViewLabirint.RowTemplate.Resizable = DataGridViewTriState.True;
+            dataGridViewLabirint.RowTemplate.Height = widthColumn;
+
+            dataGridViewLabirint.Width = (data.GetLength(1) + 1) * (widthColumn);
+            dataGridViewLabirint.Height = (data.GetLength(0) + 1) * (widthColumn);
+
+            this.Width = dataGridViewLabirint.Width;
+            this.Height = dataGridViewLabirint.Height;
+
+            
+            
+
+            for (int i = 0; i < data.GetLength(1); i++)
+            {
+               var column = new DataGridViewImageColumn();
+                column.Width = widthColumn;
+                column.ImageLayout = DataGridViewImageCellLayout.Stretch;
+                dataGridViewLabirint.Columns.Add(column);
+            }
+
+            dataGridViewLabirint.RowCount = data.GetLength(0);
+
+
+            
+
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                for (int j = 0; j < data.GetLength(1); j++)
+                {
+                    switch (data[i, j])
+                    {
+                        case 'x':
+                            dataGridViewLabirint.Rows[i].Cells[j].Value = stone; break;
+                        case '0':
+                            dataGridViewLabirint.Rows[i].Cells[j].Value = fon; break;
+                            //dataGridViewLabirint[x, y].Value = fon; 
+                    }
+
+                }
+            }
+        }
+
+        public FormGame()
+        {
+            InitializeComponent();
+        }
+
+        private void FormGame_Load(object sender, EventArgs e)
+        {
+            fon = Bitmap.FromFile("images/fon.jpg");
+            stone = Bitmap.FromFile("images/stone.jpg");
+            steve = Bitmap.FromFile("images/Steve.jpg");
+
+            field = LoadLevelFromFile("levels/level1.txt");
+            
+
+            ShowField(field);
+            dataGridViewLabirint[x, y].Value = steve;
+        }
+    }
+}
